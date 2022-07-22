cff08a3158ab11969901f4843419e08fa5f24c47
Grace <gracenyangayi@gmail.com>
2022-07-22 16:10:26 +0300

    create home component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/NavBar";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from "./components/Home";
 
 
 function App() {
@@ -9,7 +10,10 @@ function App() {
       <div>
         <Navbar/>
       </div>
-
+      <Routes>
+        <Route exact path="/" element={<Home/>}></Route>
+        
+      </Routes>
       
 
     </Router>
