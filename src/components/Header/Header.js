42b7080639867b56d9302e1b84fe7ac4c76ec437
Grace <gracenyangayi@gmail.com>
2022-07-23 23:00:59 +0300

    add header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(){
+    return(
+        // valid jsx 
+        <div className="header">
+            <span className="title">Search Me!</span>
+        </div>
+
+    )
+}
+
+export default Header
