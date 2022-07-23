219d0ee42c400458899455f17092f2f1d48f64b4
Grace <gracenyangayi@gmail.com>
2022-07-23 22:24:46 +0300

    fetch data and log to console

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import  Image  from "./Book.jpg";
 import Navbar from "./components/NavBar";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./components/Home";
 
-
+const DICTIONARY_API = "https://api.dictionaryapi.dev/api/v2/entries/en/plane" 
 function App() {
+
+
+  function fetchData(){
+    fetch(DICTIONARY_API)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
+  useEffect(
+    fetchData, []
+  )
   return (
+    <div  style={{ 
+      backgroundImage: `url(${Image})`, backgroundRepeat:"no-repeat", backgroundSize:"contain",
+      height:600,width:600
+    }}>
     <Router>
       <div>
+      
         <Navbar/>
       </div>
       <Routes>
@@ -17,6 +34,7 @@ function App() {
       
 
     </Router>
+    </div>
    
   );
 }
