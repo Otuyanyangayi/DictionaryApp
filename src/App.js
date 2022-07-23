a42ea56825b16103d3a5cf8e40b58db245c24955
Grace <gracenyangayi@gmail.com>
2022-07-23 22:33:25 +0300

    add meanings to state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import  Image  from "./Book.jpg";
 import Navbar from "./components/NavBar";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./components/Home";
 
-const DICTIONARY_API = "https://api.dictionaryapi.dev/api/v2/entries/en/plane" 
+const DICTIONARY_API = "https://api.dictionaryapi.dev/api/v2/entries/en/good" 
 function App() {
+
+  const [meanings, setMeanings ] = useState([])
 
 
   function fetchData(){
     fetch(DICTIONARY_API)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      console.log(data)
+      setMeanings(data)
+    })
   }
+
+  console.log(meanings)
 
   useEffect(
     fetchData, []
