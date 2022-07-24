77265b8c224abf104854412cc9d7603afd4b733d
Grace <gracenyangayi@gmail.com>
2022-07-24 23:43:00 +0300

    create definitions component

---
+++ src/components/Definitions/Definitions.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Definitions.css"
+
+function Definitions(){
+    return(
+        <div>
+            hello world 
+        </div>
+    )
+}
+
+export default Definitions
