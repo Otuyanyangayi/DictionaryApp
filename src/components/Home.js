eb1e17a56c67976c6e8b58d5875fa5ac14083194
Grace <gracenyangayi@gmail.com>
2022-07-24 22:59:10 +0300

    add project description

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
+import { textAlign } from "@mui/system";
 import React from "react";
 
 function Home(){
     return(
         <div>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+        <p style={{textAlign : "center"}}> 
+            This is a simple reactjs Dictionary application that allows the user to search for 
+            words and the get the meanings. It also includes the words pronnunsiation , 
+            synonyms and antonyms.
+        </p>
         </div>
     )
 }
