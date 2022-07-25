7ac47235aff1745464a53fb0f607ea54bbb03dba
Grace <gracenyangayi@gmail.com>
2022-07-25 15:44:59 +0300

    get definitions

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -1,4 +1,4 @@
-import { Difference } from "@mui/icons-material";
+
 import React from "react";
 import "./Definitions.css"
 
@@ -9,37 +9,32 @@ function Definitions({word, category, meanings }){
             {word === "" ? (
             <span className="subTitle"> Start by typing of word in Search</span>
             ) : (
-               meanings.map((mean) => 
-               mean.meanings.map((item) => 
-                // map through definitions 
-                item.definitions.map((def) => (
-                    <div className="singleMean" style={{backgroundColor: "white", color: "black"}}>
-                        <b>{def.definition}</b>
-                        <hr style={{backgroundColor: "black" ,width: "100%"}}/>
-                        {def.example &&(
-                            <san>
-                                <b>Example : </b>
-                                {def.example}
-                            </san>
-                        )}
-                        {def.synonyms && (
-                            <san>
-                            <b>Synonyms : </b>
-                            {def.synonyms}
-                        </san>
-                        )}
-                        {def.antonyms && (
-                            <san>
-                            <b>Antonyms : </b>
-                            {def.antonyms}
-                        </san>
-                        )}
-
+                meanings.map((mean) =>
+                mean.meanings.map((item) =>
+                  item.definitions.map((def) => (
+                    <div
+                      className="singleMean" style={{backgroundColor : "white", color: "black"}}>
+                      <b>{def.definition}</b>
+                      <hr style={{ backgroundColor: "black", width: "100%" }} />
+                      {def.example && (
+                        <span>
+                          <b>Example :</b> {def.example}
+                        </span>
+                      )}
+                      { def.synonyms && (
+                        <span>
+                          <b>Synonyms :</b> {def.synonyms.map((s) => `${s}, `)}
+                        </span>
+                      )}
+                      {def.antonyms && (
+                        <span>
+                          <b>Antonyms :</b> {def.antonyms.map((a) => `${a}, `)} 
+                        </span>
+                      )}
                     </div>
-                ))
-
-               ))
-                // console.log(meaning)
+                  ))
+                )
+              )                
                
             )}   
         </div>
