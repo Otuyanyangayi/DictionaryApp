51b0a204b4e52857b420c7659a48b7648e7a4370
Grace <gracenyangayi@gmail.com>
2022-07-25 12:11:57 +0300

    get definitions

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -1,10 +1,47 @@
+import { Difference } from "@mui/icons-material";
 import React from "react";
 import "./Definitions.css"
 
-function Definitions(){
+function Definitions({word, category, meanings }){
     return(
-        <div>
-            hello world 
+        <div className="meanings">
+            
+            {word === "" ? (
+            <span className="subTitle"> Start by typing of word in Search</span>
+            ) : (
+               meanings.map((mean) => 
+               mean.meanings.map((item) => 
+                // map through definitions 
+                item.definitions.map((def) => (
+                    <div className="singleMean" style={{backgroundColor: "white", color: "black"}}>
+                        <b>{def.definition}</b>
+                        <hr style={{backgroundColor: "black" ,width: "100%"}}/>
+                        {def.example &&(
+                            <san>
+                                <b>Example : </b>
+                                {def.example}
+                            </san>
+                        )}
+                        {def.synonyms && (
+                            <san>
+                            <b>Synonyms : </b>
+                            {def.synonyms}
+                        </san>
+                        )}
+                        {def.antonyms && (
+                            <san>
+                            <b>Antonyms : </b>
+                            {def.antonyms}
+                        </san>
+                        )}
+
+                    </div>
+                ))
+
+               ))
+                // console.log(meaning)
+               
+            )}   
         </div>
     )
 }
