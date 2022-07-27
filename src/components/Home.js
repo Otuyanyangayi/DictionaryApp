e1387bda0215bf19541fcf7412c8f8a30da7be3c
Grace <gracenyangayi@gmail.com>
2022-07-27 12:44:00 +0300

    update styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,15 +4,20 @@ import Background from "../assets/Best-Reviewed-Books.jpeg"
 
 function Home() {
   return (
+    <div>
+      
+      <p className="span"> This is a simple reactjs Dictionary application that allows the user to search for 
+            words and the get the meanings. It also includes the words pronunciation , 
+            synonyms and antonyms. </p>
+    
     <div className="home"
+    
       style={{
-        backgroundImage: `url(${Background})`, backgroundRepeat:"no-repeat",backgroundSize:"cover", 
-        height:"auto",width:"100%"
+        backgroundImage: `url(${Background})`, 
       }}
     >
-      <span className="span"> This is a simple reactjs Dictionary application that allows the user to search for 
-            words and the get the meanings. It also includes the words pronnunsiation , 
-            synonyms and antonyms. </span>
+      
+    </div>
     </div>
   );
 }
