a69ff7e56d937c21a5181d5881c4e20a2a96e059
Grace <gracenyangayi@gmail.com>
2022-07-28 12:38:38 +0300

    add bookmark component

---
+++ src/components/Bookmarks/Bookmarks.js
@@ -0,0 +1,54 @@
+
+import React, {  useEffect, useState } from "react";
+
+
+// deletes the bookmarks from the array.
+function deleteBookmark(e) {
+    let k=e.target.getAttribute('data-key');
+    let items=localStorage.getItem("bookmaks");
+    items=JSON.parse(items);
+    items.bookmarks.splice(k, 1);
+    localStorage.setItem("bookmaks",JSON.stringify(items)); 
+    console.log(items.bookmarks);
+    window.location.reload();
+}
+
+function Bookmarks(){
+    const [bookmarks, setBookmarks] = useState([])
+    
+function getBookmarks() {
+    let items=localStorage.getItem("bookmaks");
+    items=JSON.parse(items);
+    console.log(items)
+        setBookmarks(items.bookmarks);
+
+    }
+
+    useEffect(
+        getBookmarks, []
+    )
+    return(
+        <div>
+            <h3>Bookmarks</h3>
+            <a href="/DictionaryApp/search">Add Bookmark</a>
+           <table className="table table-striped" border="1" cellspacing="0" cellpadding="0">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+           {bookmarks.map((b,index) => (
+              <tr      key={b.name}>
+                <td>{b.name}</td>
+                <td><button onClick={deleteBookmark} data-key={index}>Delete</button></td>
+              </tr>
+            ))}
+            </tbody>
+            </table>               
+        </div>
+    )
+}
+
+export default Bookmarks
