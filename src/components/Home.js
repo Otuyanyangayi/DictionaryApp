440fd9d5d4a7e3b66836b816aec94e3bccfcd094
Grace <gracenyangayi@gmail.com>
2022-07-28 09:14:45 +0300

    update home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,19 @@
 import React from "react";
 import "./Home.css";
-import Background from "../assets/Best-Reviewed-Books.jpeg"
+
 
 function Home() {
   return (
     <div>
+      <p  className="span">
+        {" "}
+        <b>
+        This is a simple reactjs Dictionary application that allows the user to
+        search for words and the get the meanings. It also includes the words
+        pronunciation , synonyms and antonyms.</b>{" "}
+      </p>
+
       
-      <p className="span"> This is a simple reactjs Dictionary application that allows the user to search for 
-            words and the get the meanings. It also includes the words pronunciation , 
-            synonyms and antonyms. </p>
-    
-    <div className="home"
-    
-      style={{
-        backgroundImage: `url(${Background})`, 
-      }}
-    >
-      
-    </div>
     </div>
   );
 }
